9c315c3fb785517458bf82d877f5658e25134863
RadowanTri <radowan.bhuiyan.tri@gmail.com>
2026-04-19 20:33:48 +0600

    deploy

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -113,7 +113,7 @@ const Hero = () => {
       {/* Navbar */}
       <nav className="z-50 flex items-center justify-between w-full py-4 px-6 md:px-16 lg:px-24 xl:px-40 text-sm">
         <a href="#">
-            <img src="/logo.svg" alt="Logo" className="h-11 w-auto"/>
+            <img src="./logo.svg" alt="Logo" className="h-11 w-auto"/>
         </a>
 
         <div className="hidden md:flex items-center gap-8 transition duration-500 text-slate-800">
